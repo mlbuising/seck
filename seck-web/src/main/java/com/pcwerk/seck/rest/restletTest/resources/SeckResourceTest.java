7259e523a70a0fc44bb42f7934a073fec74e1ad9
John Tran <jtran-github@pcwerk.com>
2012-12-05 10:38:35 -0800

    Roll back try #2
    Apparently we had a bad commit and things went completely foobar.
    Rolling things back to get things working again.

--- seck-web/src/main/java/com/pcwerk/seck/rest/restletTest/resources/SeckResourceTest.java
+++ seck-web/src/main/java/com/pcwerk/seck/rest/restletTest/resources/SeckResourceTest.java
@@ -8,14 +8,8 @@ import org.restlet.ext.freemarker.TemplateRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
-<<<<<<< HEAD
-
-
-import com.pcwerk.seck.rest.restlet.SeckWebRestletApp;
-=======
 import org.json.*;
 
->>>>>>> 4f26047c6c2855b1e815bd479ee3cfdbc1dfe095
 import com.pcwerk.seck.rest.restletTest.SeckWebRestletAppTest;
 
 public class SeckResourceTest extends ServerResource{
@@ -24,10 +18,6 @@ public class SeckResourceTest extends ServerResource{
 		// Return data for HTML Freemarker representation
 		Map<String, Object> map = new HashMap<String, Object>();
 
-<<<<<<< HEAD
-
-		return toRepresentation(map, "seck.html", MediaType.TEXT_HTML);
-=======
 		//JSONObject jsRequest = new JSONObject();
 		JSONObject object = new JSONObject();
 		
@@ -44,23 +34,15 @@ public class SeckResourceTest extends ServerResource{
 		System.out.println(object);
 		return toRepresentation(object, "home.html", MediaType.TEXT_HTML);
 		
->>>>>>> 4f26047c6c2855b1e815bd479ee3cfdbc1dfe095
 	}
 	
 	public SeckWebRestletAppTest getApplication() {
 		return (SeckWebRestletAppTest) super.getApplication();
 	}
-<<<<<<< HEAD
-	private Representation toRepresentation(Map<String, Object> map,
-			String templateName, MediaType mediaType) {
-		return new TemplateRepresentation(templateName, getApplication()
-				.getConfiguration(), map, mediaType);
-=======
 	private Representation toRepresentation(JSONObject js,
 			String templateName, MediaType mediaType) {
 		return new TemplateRepresentation(templateName, getApplication()
 				.getConfiguration(), js, mediaType);
->>>>>>> 4f26047c6c2855b1e815bd479ee3cfdbc1dfe095
 	}
 
 }
