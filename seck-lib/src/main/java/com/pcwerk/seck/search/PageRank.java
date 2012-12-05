825679ac4e6df134e6bc6cce5de8e15b07162850
John Tran <johnjtran@acm.org>
2012-12-04 20:33:54 -0800

    Revert "removing duplicate dependencies"
    This reverts commit 4f26047c6c2855b1e815bd479ee3cfdbc1dfe095.
    
    Conflicts:
    
    	seck-web/pom.xml

---
+++ seck-lib/src/main/java/com/pcwerk/seck/search/PageRank.java
@@ -0,0 +1,12 @@
+package com.pcwerk.seck.search;
+
+import java.util.LinkedHashMap;
+
+public class PageRank implements RankingMechanism{
+
+	public LinkedHashMap<String, Double> rank(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
